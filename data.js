d05cfb7b5963be76035344d3ddfb4e53cba7c8b4
Ashish Rana <lalitakumari39840@gmail.com>
2022-05-15 22:08:56 +0530

    take it

---
+++ data.js
@@ -0,0 +1,29 @@
+var investor=[
+    {
+        avt:"https://masai-website-images.s3.ap-south-1.amazonaws.com/Vijay_Shekhar_Sharma_Paytm_96f52579b8.jpg",
+        name:"Vijay Shekhar Sharma",
+        position:"Founder & CEO",
+        logo:"https://masai-website-images.s3.ap-south-1.amazonaws.com/paytm_rect_06df45a24f.svg",
+    },
+
+    {
+        avt:"https://masai-website-images.s3.ap-south-1.amazonaws.com/Rectangle_427_637d69bb57.png",
+        name:"Kunal Shah",
+        position:"Founder",
+        logo:"https://masai-website-images.s3.ap-south-1.amazonaws.com/cred_0a47d7b4f8.png",
+    },
+
+    {
+        avt:"https://masai-website-images.s3.ap-south-1.amazonaws.com/amrish_rau_pine_labs_144e5e587c.jpg",
+        name:"Amrish Rau",
+        position:"CEO",
+        logo:"https://masai-website-images.s3.ap-south-1.amazonaws.com/pine_labs_0aa2407434.png",
+    },
+
+    {
+        avt:"https://masai-website-images.s3.ap-south-1.amazonaws.com/Sampad_swain_Instamojo_f12bf6341e.jpg",
+        name:"Sampad Swain",
+        position:"Co-Founder & CEO",
+        logo:"https://masai-website-images.s3.ap-south-1.amazonaws.com/instamojo_8763e704f7.png",
+    },
+]
